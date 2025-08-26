4f975b949d0774225dd1325cbd1ce221399d61ae
mark wonnacott <ragzouken@gmail.com>
2025-08-26 13:38:18 +0200

    feature: option to report connection health with `suggested.report.connection.health` (#81)

--- Assets/NanoverImd/NanoverImdConnectionHealth.cs
+++ Assets/NanoverImd/NanoverImdConnectionHealth.cs
@@ -1,6 +1,6 @@
-using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Text = TMPro.TextMeshProUGUI;
 
 namespace NanoverImd
@@ -14,15 +14,31 @@ namespace NanoverImd
         private Text label;
 #pragma warning restore 0649
 
+        public bool ReportToState = false;
+
         public int Frame;
         public int State;
         public int RTT;
 
         private void Update()
         {
+            UpdateSuggestedParameters();
+
             Frame = CheckTimes(simulation.Trajectory.MessageReceiveTimes);
             State = CheckTimes(simulation.Multiplayer.MessageReceiveTimes);
             RTT = Mathf.RoundToInt(simulation.Multiplayer.LastIndexRTT * 1000);
+
+            if (ReportToState)
+            {
+                var id = simulation.Multiplayer.AccessToken;
+
+                simulation.Multiplayer.SetSharedState($"report.connection.health.{id}", new Dictionary<string, object>
+                {
+                    { "frame", Frame },
+                    { "state", State },
+                    { "rtt", RTT },
+                });
+            }
 
             label.text = $"Frame: {Frame}/s, State: {State}/s, RTT: {RTT}ms";
 
@@ -40,6 +56,16 @@ namespace NanoverImd
                 }
 
                 return count;
+            }
+        }
+
+        private void UpdateSuggestedParameters()
+        {
+            const string reportKey = "suggested.report.connection.health";
+
+            if (simulation.Multiplayer.GetSharedState(reportKey) is bool report)
+            {
+                ReportToState = report;
             }
         }
     }
