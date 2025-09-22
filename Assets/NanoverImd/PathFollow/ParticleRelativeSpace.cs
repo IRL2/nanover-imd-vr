61bf401499821219c01f56adfba754e675aa9d28
mark wonnacott <ragzouken@gmail.com>
2025-09-22 16:31:05 +0200

    support reference particles as lists of particles to take centroid of

--- Assets/NanoverImd/PathFollow/ParticleRelativeSpace.cs
+++ Assets/NanoverImd/PathFollow/ParticleRelativeSpace.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 namespace NanoverImd.PathFollower
 {
@@ -15,7 +16,7 @@ namespace NanoverImd.PathFollower
         private Transform referenceSphereTemplate;
 #pragma warning restore 0649
 
-        public List<uint> particleIds = new List<uint>(); 
+        public List<List<uint>> particleIds = new List<List<uint>>(); 
         
         private List<Transform> referenceSpheres = new List<Transform>();
 
@@ -68,9 +69,9 @@ namespace NanoverImd.PathFollower
                 return;
             }
 
-            var A = frame.ParticlePositions[particleIds[0]];
-            var B = frame.ParticlePositions[particleIds[1]];
-            var C = frame.ParticlePositions[particleIds[2]];
+            var A = ComputeCentroid(particleIds[0]);
+            var B = ComputeCentroid(particleIds[1]);
+            var C = ComputeCentroid(particleIds[2]);
 
             var ABu = (B - A).normalized;
             var ACu = (C - A).normalized;
@@ -82,19 +83,41 @@ namespace NanoverImd.PathFollower
 
             transform.localPosition = center;
             transform.localRotation = Quaternion.LookRotation(z, y);
+
+            Vector3 ComputeCentroid(IList<uint> particleIds)
+            {
+                var centroid = Vector3.zero;
+
+                for (int i = 0; i < particleIds.Count; ++i)
+                    centroid += frame.ParticlePositions[particleIds[i]];
+
+                return centroid / particleIds.Count;
+            }
         }
 
         private void UpdateSuggestedParameters()
         {
             const string particlesKey = "suggested.reference.particles";
 
-            if (simulation.Multiplayer.GetSharedState(particlesKey) is IList<object> ids)
+            if (simulation.Multiplayer.GetSharedState(particlesKey) is IList<object> pointset)
             {
-                particleIds = ids.Select(id => Convert.ToUInt32(id)).ToList();
+                particleIds = pointset.Select(ConvertPoint).ToList();
+
+                List<uint> ConvertPoint(object point)
+                {
+                    if (point is IList<object> ids)
+                    {
+                        return ids.Select(id => Convert.ToUInt32(id)).ToList();
+                    }
+                    else
+                    {
+                        return new List<uint> { Convert.ToUInt32(point) };
+                    }
+                }
             }
             else
             {
-                particleIds = new List<uint>();
+                particleIds = new List<List<uint>>();
             }
         }
 
@@ -113,7 +136,17 @@ namespace NanoverImd.PathFollower
             for (int i = 0; i < count; ++i)
             {
                 referenceSpheres[i].gameObject.SetActive(true);
-                referenceSpheres[i].localPosition = frame.ParticlePositions[particleIds[i]];
+                referenceSpheres[i].localPosition = ComputeCentroid(particleIds[i]);
+            }
+
+            Vector3 ComputeCentroid(IList<uint> particleIds)
+            {
+                var centroid = Vector3.zero;
+
+                for (int i = 0; i < particleIds.Count; ++i)
+                    centroid += frame.ParticlePositions[particleIds[i]];
+
+                return centroid / particleIds.Count;
             }
         }
     }
