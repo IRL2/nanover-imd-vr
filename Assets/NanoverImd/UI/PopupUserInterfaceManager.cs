61d8182b98826ebe2eeb359b3fa0712083605b89
luis toledo <luisernesto.toledo@gmail.com>
2026-01-15 19:01:02 +0100

    show/hide ui when pressing menu button + add gaze following

--- Assets/NanoverImd/UI/PopupUserInterfaceManager.cs
+++ Assets/NanoverImd/UI/PopupUserInterfaceManager.cs
@@ -31,38 +31,36 @@ namespace NanoverImd.UI
         private InputDeviceCharacteristics characteristics;
 
         [SerializeField]
-        private bool uiVisible = false;
+        private bool uiVisible;
 
         [SerializeField]
         private Nanover.Frontend.Input.IButton menuButton;
+
+        private bool menuButtonPrevPressed;
 
         private void Start()
         {
             Assert.IsNotNull(menuPrefab, "Missing menu prefab");
 
-            //menuButton = InputDeviceCharacteristics.Left.WrapUsageAsButton(CommonUsages.menuButton);
-            //menuButton.Pressed += () => ToggleMenu();
+            uiVisible = false;
+            menuButtonPrevPressed = false;
+
             UpdatePressedInBackground().Forget();
 
             async UniTask UpdatePressedInBackground()
             {
-                var openMenu = new DirectButton();
-                openMenu.Pressed += ShowMenu;
-                openMenu.Released += CloseMenu;
-
                 while (true)
                 {
                     try
                     {
-                        var joystick = characteristics.GetFirstDevice().GetJoystickValue(CommonUsages.primary2DAxis) ?? Vector2.zero;
-                        var pressed = Mathf.Abs(joystick.y) > .5f;
+                        var pressed = characteristics.GetFirstDevice().GetButtonPressed(CommonUsages.menuButton) == true;
 
-                        //var pressed = characteristics.GetFirstDevice().GetButtonPressed(CommonUsages.menuButton) == true;
+                        if (pressed && !menuButtonPrevPressed)
+                        {
+                            ToggleMenu();
+                        }
 
-                        if (pressed && !openMenu.IsPressed)
-                            openMenu.Press();
-                        else if (!pressed && openMenu.IsPressed)
-                            openMenu.Release();
+                        menuButtonPrevPressed = pressed;
                     }
                     catch (System.Exception e)
                     {
@@ -81,16 +79,16 @@ namespace NanoverImd.UI
         {
             if (!controllers.WouldBecomeCurrentMode(mode))
                 return;
-            
+
             GotoScene(menuPrefab);
-            
-            SceneUI.transform.position = SceneUI.GetComponent<PhysicalCanvasInput>()
-                                                .Controller.transform.position + offset;
+
+            SceneUI.transform.position = Camera.main.transform.position +
+                                         Vector3.down * 0.2f +
+                                         Camera.main.transform.forward * 0.7f;
+
             SceneUI.transform.rotation =
                 Quaternion.LookRotation(SceneUI.transform.position - Camera.main.transform.position,
                                         Vector3.up);
-
-            uiVisible = true;
         }
 
         private void CloseMenu()
@@ -98,17 +96,16 @@ namespace NanoverImd.UI
             if (clickOnMenuClosed)
                 WorldSpaceCursorInput.TriggerClick();
             CloseScene();
-
-            uiVisible = false;
         }
 
         private void ToggleMenu()
         {
-            Debug.Log("Toggling menu");
+            uiVisible = !uiVisible;
+
             if (uiVisible)
-                CloseMenu();
-            else
                 ShowMenu();
+            else
+                CloseMenu();
         }
     }
 }
