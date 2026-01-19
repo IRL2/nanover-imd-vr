4cc69b87fa6b924726a6ce6e3619749d5af4acb0
luis toledo <luisernesto.toledo@gmail.com>
2026-01-19 18:25:26 +0100

    Enable lefthand interaction ray Fix 1st-time connecting not enabling simulation-interaction mode Add nanover logo on initial screen

--- Assets/NanoverImd/UI/UserInterfaceManager.cs
+++ Assets/NanoverImd/UI/UserInterfaceManager.cs
@@ -1,7 +1,12 @@
 using System.Collections.Generic;
 using System.Linq;
+using Cysharp.Threading.Tasks;
+using Nanover.Frontend.Controllers;
+
 using Nanover.Frontend.UI;
+using Nanover.Frontend.XR;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace NanoverImd.UI
 {
@@ -18,7 +23,17 @@ namespace NanoverImd.UI
         [SerializeField]
         private GameObject sceneUI;
 
+        [SerializeField]
+        private GameObject simulation;
+
         private Stack<GameObject> sceneStack = new Stack<GameObject>();
+
+        [SerializeField]
+        private InputDeviceCharacteristics characteristics;
+
+
+        private bool uiVisible;
+        private bool menuButtonPrevPressed;
 
         public GameObject SceneUI => sceneUI;
 
@@ -26,6 +41,40 @@ namespace NanoverImd.UI
         {
             if (initialScene != null)
                 GotoScene(initialScene);
+
+
+            uiVisible = false;
+            menuButtonPrevPressed = false;
+
+            UpdatePressedInBackground().Forget();
+
+            async UniTask UpdatePressedInBackground()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var pressed = characteristics.GetFirstDevice().GetButtonPressed(CommonUsages.menuButton) == true;
+
+                        if (pressed && !menuButtonPrevPressed)
+                        {
+                            uiVisible = SceneUI.transform.gameObject.activeInHierarchy;
+                            // todo: hide full-screen ui when menubutton pressed
+                            // todo: show initialscene when no simulation is running
+                            //if (uiVisible)
+                            //    LeaveScene(SceneUI);
+                        }
+
+                        menuButtonPrevPressed = pressed;
+                    }
+                    catch (System.Exception e)
+                    {
+                        Debug.LogException(e);
+                    }
+
+                    await UniTask.DelayFrame(1);
+                }
+            }
         }
 
         private void LeaveScene(GameObject scene)
